b7b3835c263208e43146eb3c17007c328fbb8465
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-16 15:30:47 +0200

    milestone2

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image":"https://unsplash.it/300/300?image=12"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +55,48 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+/*-------------------------------------CREAZIONE POSTS-------------------------------------*/
+
+for (let index = 0; index < posts.length; index++) {
+    const myPostList = document.querySelector('.posts-list');
+
+    const newPost = `<div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${posts[index].author.image}" alt="${posts[index].author.name}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${posts[index].author.name}</div>
+                            <div class="post-meta__time">${posts[index].created}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${posts[index].content}</div>
+                <div class="post__image">
+                    <img src="${posts[index].media}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <div class="like-button  js-like-button" data-postid="${posts[index].id}">
+                                <i class="like-button__icon fas fa-thumbs-up"></i>
+                                <i class="like-button__icon fas fa-thumbs-up hidden"></i>                                
+                                <span class="like-button__label">Like!</span>
+                            </div>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-${posts[index].id}" class="js-likes-counter">${posts[index].likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>`;
+    myPostList.innerHTML += newPost;
+
+};
+
+
+
+
